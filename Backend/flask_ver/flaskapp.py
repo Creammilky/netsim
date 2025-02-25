89adc7fc4049378660a494cc3cc96e75dd127c45
Carl <Cream_milk@bupt.edu.cn>
2025-02-25 13:45:24 +0800

    update

--- Backend/flask_ver/flaskapp.py
+++ Backend/flask_ver/flaskapp.py
@@ -1,6 +1,6 @@
 from flask import Flask, jsonify, request, render_template
 from flask_cors import CORS
-from api.semantic_analyze import GraphParser
+from api.xml_parser import GraphParser
 from utils.graph_utils import InteractiveNetwork
 
 app = Flask(__name__)
@@ -12,6 +12,7 @@ interactive_net = InteractiveNetwork(G)  # G 是你的原始NetworkX图
 # 启用跨域访问
 CORS(app)
 
+
 @app.route('/update_graph', methods=['POST'])
 def update_graph():
     data = request.json
@@ -19,14 +20,17 @@ def update_graph():
     interactive_net.update_graph(action, data)
     return jsonify({"status": "success"})
 
+
 @app.route('/show_graph')
 def show_graph():
     return interactive_net.create_interactive_graph().show('network.html', notebook=False)
+
 
 @app.route('/')
 def root():
     interactive_net.create_interactive_graph().save_graph('./templates/network.html')
     return render_template('network.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- api/sdn_api.py
+++ api/sdn_api.py
@@ -1,3 +1,5 @@
 import networkx
+
 def gen_frr_topology():
     pass
+
